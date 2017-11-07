2667cce6ba18f65b51516de16ff01dd47cfb48c8
Alexander Eriksson <aleer778@student.liu.se>
2017-11-07 21:26:12 +0100

    Added network options

---
+++ ServerPrototyp/Server.h
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <iostream>
+
+class Server
+{
+public:
+    Server();
+    ~Server();
+    void Run();
+private:
+    void Init();
+    void Connect();
+
+    sf::IpAddress ipAddress;
+    int connectionPort{6789};
+};
+
