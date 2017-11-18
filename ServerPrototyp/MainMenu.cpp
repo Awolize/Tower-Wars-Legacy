b7497674407177e8115c5f0fafba34b95c2f24d3
Alexander Eriksson <aleer778@student.liu.se>
2017-11-18 15:52:16 +0100

    Added menu for Client/JoinGame

--- ServerPrototyp/MainMenu.cpp
+++ ServerPrototyp/MainMenu.cpp
@@ -1,5 +1,5 @@
-#include "Server.h"
-#include "Client.h"
+#include "ServerMenu.h"
+#include "ClientMenu.h"
 #include "MainMenu.h"
 
 #include <SFML/Graphics.hpp>
@@ -16,42 +16,22 @@ MainMenu::~MainMenu() {}
 
 void MainMenu::Run(sf::RenderWindow &window)
 {
-//    sf::Font font;
-//    font.loadFromFile("arial.ttf");
-// Host Server menuoption ractangle
     sf::RectangleShape HostMenuOption;
     HostMenuOption.setSize(sf::Vector2f(400, 100)); // fix position, size, texture, text
-    HostMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2, 
-					    window.getSize().y / 2 - 150));
+    HostMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2 - 150));
     HostMenuOption.setOrigin(sf::Vector2f(HostMenuOption.getSize() / 2.0f));
-// Host Server menuoption Text
-//    sf::Text text("Host Game", font);
-//    text.setCharacterSize(30);
-//    text.setStyle(sf::Text::Bold);
-//    text.setColor(sf::Color::Red);
 
 // Join Server menuoption ractangle
     sf::RectangleShape JoinMenuOption;
     JoinMenuOption.setSize(sf::Vector2f(400, 100)); // fix position, size, texture, text
     JoinMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2));
     JoinMenuOption.setOrigin(sf::Vector2f(JoinMenuOption.getSize() / 2.0f));
-// Join Server menuoption Text
-//    sf::Text text("Join Game", font);
-//    text.setCharacterSize(30);
-//    text.setStyle(sf::Text::Bold);
-//    text.setColor(sf::Color::Red);
 
 // Close game menuoption ractangle
     sf::RectangleShape QuitMenuOption;
     QuitMenuOption.setSize(sf::Vector2f(400, 100)); // fix position, size, texture, text
-    QuitMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2, 
-					    window.getSize().y / 2 + 150));
+    QuitMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2 + 150));
     QuitMenuOption.setOrigin(sf::Vector2f(QuitMenuOption.getSize() / 2.0f));
-// Close Server menuoption Text
-//    sf::Text text("Close", font);
-//    text.setCharacterSize(30);
-//    text.setStyle(sf::Text::Bold);
-//    text.setColor(sf::Color::Red);
 
     while(window.isOpen())
     {
@@ -63,13 +43,13 @@ void MainMenu::Run(sf::RenderWindow &window)
 	    // HostMenuOption
 	    if (   event.mouseButton.x > HostMenuOption.getPosition().x - HostMenuOption.getSize().x / 2 
 		   && event.mouseButton.x < HostMenuOption.getPosition().x + HostMenuOption.getSize().x / 2
-		   && event.mouseButton.y > HostMenuOption.getPosition().y - HostMenuOption.getSize().x / 2
+		   && event.mouseButton.y > HostMenuOption.getPosition().y - HostMenuOption.getSize().y / 2
 		   && event.mouseButton.y < HostMenuOption.getPosition().y + HostMenuOption.getSize().y / 2 )
 	    {
 		if (event.mouseButton.button == sf::Mouse::Left)
 		{
-		    Server server(window);
-		    server.Boot(window);
+		    ServerMenu serverMenu(window);
+		    serverMenu.Boot(window);
 		}
 		// Change color on text when hover?
 	    }		
@@ -81,8 +61,8 @@ void MainMenu::Run(sf::RenderWindow &window)
 	    {
 		if (event.mouseButton.button == sf::Mouse::Left)
 		{
-		    Client client;
-		    client.Boot(window);
+		    ClientMenu clientMenu(window);
+		    clientMenu.Boot(window);
 		}
 		// Change color on text when hover?
 	    }
