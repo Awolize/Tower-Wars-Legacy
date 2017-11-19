cb99c738cfbcc51202e6a3251d31c573b5c0359e
Alexander Eriksson <aleer778@student.liu.se>
2017-11-19 03:10:27 +0100

    Fixed main menu

--- ServerPrototyp/MainMenu.cpp
+++ ServerPrototyp/MainMenu.cpp
@@ -16,22 +16,65 @@ MainMenu::~MainMenu() {}
 
 void MainMenu::Run(sf::RenderWindow &window)
 {
+    sf::Text HostGameText;
+
+    sf::Text JoinGameText;
+
+    sf::Text QuitGameText;
+
+    sf::Font font;
+    font.loadFromFile("Arial.ttf");
+
+    sf::FloatRect textRect;
+
+    HostGameText.setFont(font);
+    HostGameText.setCharacterSize(30);
+    HostGameText.setFillColor(sf::Color::White);
+    HostGameText.setString("Host Game");
+    HostGameText.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2 - 150));
+
+    textRect = HostGameText.getLocalBounds();
+    HostGameText.setOrigin(textRect.left + textRect.width/2.0f,
+		     textRect.top  + textRect.height/2.0f);
+
+    JoinGameText.setFont(font);
+    JoinGameText.setCharacterSize(30);
+    JoinGameText.setFillColor(sf::Color::White);
+    JoinGameText.setString("Join Game");
+    JoinGameText.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2));
+    textRect = JoinGameText.getLocalBounds();
+    JoinGameText.setOrigin(textRect.left + textRect.width/2.0f,
+		     textRect.top  + textRect.height/2.0f);
+
+    QuitGameText.setFont(font);
+    QuitGameText.setCharacterSize(30);
+    QuitGameText.setFillColor(sf::Color::White);
+    QuitGameText.setString("Quit Game");
+    QuitGameText.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2 + 150));
+    textRect = QuitGameText.getLocalBounds();
+    QuitGameText.setOrigin(textRect.left + textRect.width/2.0f,
+		     textRect.top  + textRect.height/2.0f);
+
+
     sf::RectangleShape HostMenuOption;
     HostMenuOption.setSize(sf::Vector2f(400, 100)); // fix position, size, texture, text
     HostMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2 - 150));
     HostMenuOption.setOrigin(sf::Vector2f(HostMenuOption.getSize() / 2.0f));
+    HostMenuOption.setFillColor(sf::Color(20, 20, 20));
 
 // Join Server menuoption ractangle
     sf::RectangleShape JoinMenuOption;
     JoinMenuOption.setSize(sf::Vector2f(400, 100)); // fix position, size, texture, text
     JoinMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2));
     JoinMenuOption.setOrigin(sf::Vector2f(JoinMenuOption.getSize() / 2.0f));
+    JoinMenuOption.setFillColor(sf::Color(20, 20, 20));
 
 // Close game menuoption ractangle
     sf::RectangleShape QuitMenuOption;
     QuitMenuOption.setSize(sf::Vector2f(400, 100)); // fix position, size, texture, text
     QuitMenuOption.setPosition(sf::Vector2f(window.getSize().x / 2,  window.getSize().y / 2 + 150));
     QuitMenuOption.setOrigin(sf::Vector2f(QuitMenuOption.getSize() / 2.0f));
+    QuitMenuOption.setFillColor(sf::Color(20, 20, 20));
 
     while(window.isOpen())
     {
@@ -87,6 +130,10 @@ void MainMenu::Run(sf::RenderWindow &window)
 	window.draw(HostMenuOption);
 	window.draw(JoinMenuOption);
 	window.draw(QuitMenuOption);
+
+	window.draw(HostGameText);
+	window.draw(JoinGameText);
+	window.draw(QuitGameText);
 
   	window.display();
 	
