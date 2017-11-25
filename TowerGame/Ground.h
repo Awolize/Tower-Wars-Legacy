c1636edddc53f38f6c61bcff4295d00a93a8371e
Alexander Eriksson <aleer778@student.liu.se>
2017-11-25 18:00:10 +0100

    Changed ground, works now

---
+++ TowerGame/Ground.h
@@ -0,0 +1,25 @@
+#ifndef GROUND_H
+#define GROUND_H
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+#include <vector>
+#include <iostream>
+#include "Tower.h"
+#include "Soldier.h"
+
+class Ground
+{
+public:
+    Ground();
+    void AddTower(Tower tower);
+    void RemoveTower(Tower tower);
+
+    void AddSoldier(Soldier soldier);
+    void MoveSoldier(Soldier soldier);
+    void RemoveSoldier(Soldier soldier);
+private:
+    std::array<std::array<std::vector<int>, 14>, 10> grid;
+};
+
+#endif
