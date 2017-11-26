005d2ceb5816279f9e7754f3ba2be8cacc0de6b5
Alexander Eriksson <aleer778@student.liu.se>
2017-11-26 14:26:35 +0100

    merge

--- TowerGame/Entity.h
+++ TowerGame/Entity.h
@@ -5,24 +5,12 @@
 class Entity
 {
 public:
-<<<<<<< HEAD
     Entity();
     Entity(sf::Vector2f position);
     ~Entity();
     virtual void Update(float deltaTime) = 0;
     virtual void Draw(sf::RenderWindow &window) = 0;
     sf::RectangleShape body;
-=======
-  Entity();
-  Entity(sf::Vector2f position);
-  ~Entity();
-  
-  virtual void Update(float deltaTime) = 0;
-  virtual void Draw(sf::RenderWindow& window) = 0;
-
-  sf::RectangleShape body;
-  
->>>>>>> cf87a72393a891746e61fbdaa14f090e64f1ae07
 };
 
 #endif
