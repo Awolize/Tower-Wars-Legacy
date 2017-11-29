c0a921c1f41c8e57ef07abfde3fda95456c462d7
Kristoffer Karlsson <krika694@student.liu.se>
2017-11-29 17:17:05 +0100

    Stefan Lofwen

--- TowerGame/Soldier.cpp
+++ TowerGame/Soldier.cpp
@@ -5,6 +5,25 @@ using namespace std;
 Soldier::Soldier(int type, int index) : index(index)
 {
     Create(type);
+    tilePos[0] = sf::Vector2i(3,0);    tilePos[1] = sf::Vector2i(3,1);    tilePos[2] = sf::Vector2i(2,1);
+    tilePos[3] = sf::Vector2i(1,1);    tilePos[4] = sf::Vector2i(1,2);    tilePos[5] = sf::Vector2i(1,3);
+    tilePos[6] = sf::Vector2i(1,4);    tilePos[7] = sf::Vector2i(1,5);    tilePos[8] = sf::Vector2i(1,6);
+    tilePos[9] = sf::Vector2i(2,6);    tilePos[10] = sf::Vector2i(3,6);    tilePos[11] = sf::Vector2i(4,6);
+    tilePos[12] = sf::Vector2i(4,5);    tilePos[13] = sf::Vector2i(4,4);    tilePos[14] = sf::Vector2i(5,4);
+    tilePos[15] = sf::Vector2i(6,4);    tilePos[16] = sf::Vector2i(6,5);    tilePos[17] = sf::Vector2i(6,6);
+    tilePos[18] = sf::Vector2i(6,7);    tilePos[19] = sf::Vector2i(6,8);    tilePos[20] = sf::Vector2i(6,9);
+    tilePos[21] = sf::Vector2i(5,9);    tilePos[22] = sf::Vector2i(4,9);    tilePos[23] = sf::Vector2i(3,9);
+    tilePos[24] = sf::Vector2i(2,9);    tilePos[25] = sf::Vector2i(1,9);    tilePos[26] = sf::Vector2i(1,10);
+    tilePos[27] = sf::Vector2i(1,11);    tilePos[28] = sf::Vector2i(1,12);    tilePos[29] = sf::Vector2i(2,12);
+    tilePos[30] = sf::Vector2i(3,12);    tilePos[31] = sf::Vector2i(4,12);    tilePos[32] = sf::Vector2i(5,12);
+    tilePos[33] = sf::Vector2i(5,12);    tilePos[34] = sf::Vector2i(6,12);    tilePos[35] = sf::Vector2i(7,12);
+    tilePos[36] = sf::Vector2i(8,12);    tilePos[37] = sf::Vector2i(8,11);    tilePos[38] = sf::Vector2i(8,11);
+    tilePos[39] = sf::Vector2i(8,10);    tilePos[40] = sf::Vector2i(8,9);    tilePos[41] = sf::Vector2i(8,8);
+    tilePos[42] = sf::Vector2i(8,7);    tilePos[43] = sf::Vector2i(8,6);    tilePos[44] = sf::Vector2i(8,5);
+    tilePos[45] = sf::Vector2i(8,4);    tilePos[46] = sf::Vector2i(8,3);    tilePos[47] = sf::Vector2i(8,2);
+    tilePos[48] = sf::Vector2i(8,1);    tilePos[49] = sf::Vector2i(7,1);    tilePos[50] = sf::Vector2i(6,1);
+    tilePos[51] = sf::Vector2i(6,0);
+
 }
 
 
@@ -18,6 +37,10 @@ void Soldier::Update(float deltaTime)
 //	body.setTextureRect(animation.xyRect);
 	//body.move(deltaTime);
     }
+    sf::Time time = moveClock.getElapsedTime();
+    if(time.asMilliseconds() >= 400)
+	MoveOneTile();
+    Logic();
   
 }
 
@@ -28,11 +51,19 @@ void Soldier::Draw(sf::RenderWindow& window)
     {
 	window.draw(body);
     }
-
 }
 
-void Soldier::Intelligence(sf::Vector2f Distance)
+void Soldier::MoveOneTile()
 {
+    tileNumber++;
+    moveClock.restart();
+    
+}
+
+void Soldier::Logic()
+{
+
+    
 }
 
 void Soldier::Create(int type)
@@ -49,6 +80,8 @@ void Soldier::Create(int type)
 	row = 0;
 	income = 2;
 	moveSpeed = 2;
+	tileNumber = 0;
+	
     }
     if(type == 2) // not done
     {
@@ -61,7 +94,9 @@ void Soldier::Create(int type)
 	row = 0;
 	income = 2;
 	moveSpeed = 2;
+	tileNumber = 0;
     }
+    moveClock.restart();
     body.setTexture(&texture);
     body.setPosition(sf::Vector2f(10,10));
 
